b3ddc6734d09533f1f86f2ac7c046fceb12df341
MUMIN <abdulmuminjerima@gmail.com>
2022-06-30 16:07:13 -0700

    use puts to print output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/
+* main - this is my main function with no arguments
+*this function must output a success
+*/ 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
