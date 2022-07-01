c48fbbc8305572a1c5c29ca7d6dea6faa330aa67
MUMIN <abdulmuminjerima@gmail.com>
2022-06-30 18:48:54 -0700

    sizeof of data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,11 @@
  */
 int main(void)
 {
-	int i;
-	long int l;
-	long long int ll;
-	float d;
-	char c;
-
-	printf("size of a char: %zu byte(s)\n", sizeof(c));
-	printf("Size of an int: %zu byte(s)\n", sizeof(i));
-	printf("size of a long int: %zu byte(s)\n", sizeof(l));
-	printf("size of a long long int: %zu byte(s)\n", sizeof(ll));
-	printf("size of a float: %zu byte(s)\n", sizeof(d));
+	
+	printf("size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
