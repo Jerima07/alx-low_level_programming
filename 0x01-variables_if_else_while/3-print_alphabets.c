878a6802b40b50dcaacd0d3071c77305a6d05355
MUMIN <abdulmuminjerima@gmail.com>
2022-07-01 21:06:31 -0700

    print both lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - printing both lower and upper case
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int alp;
+
+	for (alp = 97; alp <= 122; alp++)
+	{
+		putchar(alp);
+	}
+	for (alp = 65; alp<= 90; alp++)
+	{
+		putchar(alp);
+	}
+	putchar(10); 
+	return (0);
+}
