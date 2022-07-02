2280af71f6a396dc92c88b779f92e1bf386f76ca
MUMIN <abdulmuminjerima@gmail.com>
2022-07-01 19:45:00 -0700

    sizeof data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,12 +8,12 @@ int main(void)
 	char a;
 	int o;
 	long int m;
-	long long int i;
-	float n;
-	printf("Size of a char: %zu byte(s)\n", sizeof(a));
+	long long int i;;
+
+	printf("size of a char: %zu byte(s)\n", sizeof(a));
 	printf("Size of an int: %zu byte(s)\n", sizeof(o));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(m));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(i));
-	printf("Size of a float: %zu byte(s)\n", sizeof(n));
+	printf("size of a long int: %zu byte(s)\n", sizeof(m));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(i));
+	printf("size of a float: %zu byte(s)\n", sizeof(n));
 	return (0);
 }
