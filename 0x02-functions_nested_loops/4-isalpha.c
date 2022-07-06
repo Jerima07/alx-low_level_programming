9ff5e1e8a16ecb3bc712a45ccd2deec1ceeeb98f
MUMIN <abdulmuminjerima@gmail.com>
2022-07-06 08:01:18 -0700

    function to check for alphabet characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha -fuction to check for alphabet character
+ * @c: is the argument of the funtion
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
