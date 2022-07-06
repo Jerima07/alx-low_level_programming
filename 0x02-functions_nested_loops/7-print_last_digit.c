2fa75c9a11928a85e2368bd2deebb0eadc329882
MUMIN <abdulmuminjerima@gmail.com>
2022-07-06 08:40:17 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,14 @@ int print_last_digit(int n)
 	int j;
 
 	if (n < 0)
-	{
-		n = -n;
-		j = n % 10;
-		if (j < 0)
-		{
-			j = -j;
-		}
-		_putchar(j + '0');
-		return (j);
-	}
+	
+	n = -n;
+	j = n % 10;
+	if (j < 0)
+		
+	j = -j;
+		
+	_putchar(j + '0');
+	return (j);
+	
 }
