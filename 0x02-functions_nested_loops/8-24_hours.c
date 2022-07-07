3a1e31dcd670ee49f868383326adc7c08d3691eb
MUMIN <abdulmuminjerima@gmail.com>
2022-07-07 05:44:40 -0700

    function to print all minutes of jack beaur 24hrs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function to print every minute of jack beur 24hrs
+ * n from 0:00 t0 23:59
+ */
+void jack_bauer(void)
+{
+	int h = 0;
+	int m = 0;
+	int h_rem;
+	int m_rem;
+
+	while (h <= 23)
+	{
+		while (m <= 59)
+		{
+			m_rem = m % 10;
+			h_rem = h % 10;
+			-putchar(h / 10 + '0');
+			_putchar(h_rem + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m_rem + '0');
+			m++;
+			-putchar('\n');
+		}
+		h++;
+		m = 0;
+	}
+}
