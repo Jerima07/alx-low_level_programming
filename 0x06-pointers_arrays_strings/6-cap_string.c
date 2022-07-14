d63a5fed848874e932abb2dd69a68ee50097b854
MUMIN <abdulmuminjerima@gmail.com>
2022-07-14 10:56:21 -0700

    change to caps

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string -convert all to caps
+ * @s: sting input
+ * Return: pointer
+ */
+char *cap_string(char *s)
+{
+	int count = 0, i;
+	int sep_wrds[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 13; i++)
+		{
+			if (*(s + count) == sep_wrds[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
+		}
+		count++;
+	}
+	return (s);
+}
