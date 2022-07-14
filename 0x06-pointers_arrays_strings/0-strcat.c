73b9cb70f7a1497a9bf6eaed21b98dcf7fdad382
MUMIN <abdulmuminjerima@gmail.com>
2022-07-14 08:51:26 -0700

    concat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,20 +8,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int count = 0, count1 = 0;
+	int count = 0, count2 = 0;
 
 	while (*(dest + count) != '\0')
 	{
 		count++;
 	}
 
-	while (count1 >= 0)
+	while (count2 >= 0)
 	{
-		*(dest + count) = *(src + count1);
-		if (*(src + count1) == '\0')
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
 			break;
 		count++;
-		count1++;
+		count2++;
 	}
 	return (dest);
 }
