ee0640ff36f55dd4f199b34754f8ed5709d34276
MUMIN <abdulmuminjerima@gmail.com>
2022-08-01 15:33:53 -0700

    print filr name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main -check code for school students
+ * main -print file name
  * Return: Always 0
  */
 int main(void)
 {
-	printf("%\n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
