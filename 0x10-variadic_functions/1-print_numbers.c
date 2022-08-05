b681bfd3f708d774a37cf8a2fd12eaaee25faff2
MUMIN <abdulmuminjerima@gmail.com>
2022-08-05 06:10:28 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,7 +11,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list valist;
 	unsigned int i;
 
-	var_start(valist, n);
+	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
 	{
